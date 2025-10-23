3b90a2aafb200bb90f4e15969f214ec5971b61a1
Christian Tica <ticachristian20@gmail.com>
2025-10-23 10:03:01 +0300

    added build directories in gitignore

---
+++ kokkos/01-simple-axpy/job_lumi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=slurm-test
+#SBATCH --account=project_462001074
+#SBATCH --partition=small-g
+# #SBATCH --reservation=portgp-2025-wed # This changes every day to -wed, -thu and -fri, valid 09:00 to 17:00
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=1
+
+#srun ./build-omp/hello --kokkos-print-configuration 
+srun ./build-hip/hello --kokkos-print-configuration 
